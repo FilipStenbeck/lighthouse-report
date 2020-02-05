086f76e3b9b9a2b126c2299d6ddaffe92d3a77d1
Filip Stenbeck <filip.stenbeck@gmail.com>
2020-02-05 11:46:23 +0100

    feat: automatic deploy to prod

--- .aks/generate.sh
+++ .aks/generate.sh
@@ -6,9 +6,8 @@ TO=app.yaml
 rm -rf app.yaml
 
 APP_VERSION=$(cat ../package.json | jq .version | jq -r)
-APP_NAME=$(cat ../package.json | jq .name | jq -r)
-
-
+APP_NAME=$(cat ../package.json | jq .name | jq -r | cut -d "/" -f2)
+ 
 echo Generating $TO
 export ENVIRONMENT=$1
 export DOCKER_IMAGE=hi3gcontainerregistry.azurecr.io/$APP_NAME:$APP_VERSION

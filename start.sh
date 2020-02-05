3e52d64c44bac3b2c07017b40c0795d7cd5570ba
Filip Stenbeck <filip.stenbeck@gmail.com>
2020-02-05 09:03:07 +0100

    chore: add gitignore

--- start.sh
+++ start.sh
@@ -5,5 +5,7 @@ do
   # ADD URLS TO TEST HERE #
   ./lighthouse.sh https://www.tre.se/support
   ./lighthouse.sh https://www.tre.se/support/\#kontaktaoss
+
+  # Set interval for the tests
   sleep 60;
 done

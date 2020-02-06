274edd77aa2e89aeec59b9af75cf466e6714811c
Filip Stenbeck <filip.stenbeck@gmail.com>
2020-02-06 07:21:40 +0100

    feat: add webshop url

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ while [ true ]
 do
   # ADD URLS TO TEST HERE #
   ./lighthouse.sh https://www.tre.se/support
-  ./lighthouse.sh https://www.tre.se/support/\#kontaktaoss
+  ./lighthouse.sh https://www.tre.se/handla/mobiltelefoner
 
   # Set interval for the tests
   sleep 60;

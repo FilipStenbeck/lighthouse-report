c6d0cab5eaaaf7d90dc22157360f927f1aa2a443
Filip Stenbeck <filip.stenbeck@gmail.com>
2020-02-11 12:56:16 +0100

    feat: add web server

--- lighthouse.sh
+++ lighthouse.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
-OUTPUT_NAME=reports/output-$(date +%Y-%m-%d:%H:%M).json
-lighthouse --quiet --chrome-flags="--headless --no-sandbox" $1 --output json --output-path ${OUTPUT_NAME};
-node buildReport.js $OUTPUT_NAME
+OUTPUT_NAME=reports/$(date +%Y-%m-%d:%H:%M)
+PAGE=$1
+SUFFIX=$2
+lighthouse --quiet --chrome-flags="--headless --no-sandbox" $1 --output json --output-path ${OUTPUT_NAME}.json;
+lighthouse --quiet --chrome-flags="--headless --no-sandbox" $1 --output html --output-path ${OUTPUT_NAME}-${SUFFIX}.html
+node buildReport.js $OUTPUT_NAME.json
--- start.sh
+++ start.sh
@@ -3,8 +3,8 @@
 while [ true ] 
 do
   # ADD URLS TO TEST HERE #
-  ./lighthouse.sh https://www.tre.se/support
-  ./lighthouse.sh https://www.tre.se/handla/mobiltelefoner
+  ./lighthouse.sh https://www.tre.se/support support
+  ./lighthouse.sh https://www.tre.se/handla/mobiltelefoner handla-mobiltelefoner
 
   # Set interval for the tests
   sleep 60;

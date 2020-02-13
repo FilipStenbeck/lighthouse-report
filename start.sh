c0f0963c943cb0c65df38e482b55c7367de4c751
Filip Stenbeck <filip.stenbeck@gmail.com>
2020-02-13 11:10:46 +0100

    feat: delete reports older than 1 day

--- start.sh
+++ start.sh
@@ -7,6 +7,9 @@ do
   ./lighthouse.sh https://www.tre.se/support support
   ./lighthouse.sh https://www.tre.se/handla/mobiltelefoner handla-mobiltelefoner
 
+  #Cleanup old files (older than 1 day)
+  find reports/ -type f -mtime +1 -exec rm -f {} \;
+
   # Set interval for the tests
   sleep 60;
 done
